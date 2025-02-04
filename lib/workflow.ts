f0e8118af1080e1784a22add27b412a205d5cb2c
thechin press <thechinpress@gmail.com>
2025-02-04 22:06:31 +0630

    email sending

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,26 @@
 import { Client as WorkflowClient } from '@upstash/workflow'
 import config from './config'
+import { Client as QstashClient, resend } from "@upstash/qstash";
 
 export const workflowClient = new WorkflowClient({
     baseUrl: config.env.upstash.qstashUrl,
     token: config.env.upstash.qstashToken!
 })
+
+const qstashClient = new QstashClient({ token: config.env.upstash.qstashToken! });
+
+export const sendEmail = async ({ email, subject, message }: { email: string, subject: string, message: string }) => {
+
+    await qstashClient.publishJSON({
+        api: {
+            name: "email",
+            provider: resend({ token: config.env.upstash.resendToken! }),
+        },
+        body: {
+            from: "University Library <test.glamproplus.com>",
+            to: [email],
+            subject: subject,
+            html: message
+        },
+    });
+}
